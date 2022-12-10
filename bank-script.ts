60dfb45a80e6257ae5299dd49481480e62584073
Corentin Ancel <corentin.ancel@edu.devinci.fr>
2022-12-10 16:20:39 +0100

    ADD user

--- bank-script.ts
+++ bank-script.ts
@@ -1,54 +1,91 @@
 export let Bank = class Bank {
-    private users: any;
-    constructor(users) {
-        this.users = users;
+  private users: any;
+  constructor(users) {
+    this.users = users;
+  }
+
+  lookBalance(cardNumber, cardCode) {
+    try {
+      let user = this.checkUser(cardNumber, cardCode);
+      return user.balance;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  addBalance(cardNumber, cardCode, money) {
+    try {
+      let user = this.checkUser(cardNumber, cardCode);
+      return user.balance + money;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  takeLoan(cardNumber, cardCode, loan) {
+    try {
+      let user = this.checkUser(cardNumber, cardCode);
+      if (loan < user.balance) {
+        return user.balance - loan;
+      }
+      return user.balance;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  addUser(user) {
+    try {
+      if (!this.checkAlreadyExist(user.cardNumber)) {
+        this.users.push(user);
+        return true;
+      }
+    } catch (err) {
+      return err;
+    }
+  }
+
+  deleteUser(user) {
+    try {
+      let thisUser = this.checkUser(user.cardNumber, user.cardCode);
+      this.users.splice(this.users.indexOf(thisUser), 1);
+      return true;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  // updateUser(user) {
+  //   try {
+  //     let thisUser = this.checkUser(user.cardNumber, user.cardCode);
+  //     thisUser.balance = user.balance;
+  //     thisUser.name = user.name;
+  //   } catch (err) {
+  //     return err;
+  //   }
+  // }
+
+  private checkUser(cardNumber, cardCode) {
+    let currentUser = null;
+    this.users.forEach((user) => {
+      if (user.cardNumber === cardNumber && user.cardCode === cardCode) {
+        currentUser = user;
+      }
+    });
+    if (currentUser !== null) {
+      return currentUser;
+    } else {
+      throw "Invalid user";
+    }
+  }
+
+  private checkAlreadyExist(cardNumber) {
+    const exists = this.users.some((user) => user.cardNumber === cardNumber);
+
+    if (exists) {
+      throw "User already exists";
     }
 
-    lookBalance(cardNumber, cardCode){
-        try {
-            let user =  this.checkUser(cardNumber, cardCode)
-            return user.balance
-        }
-        catch(err) {
-            return err
-        }
-    }
-
-    addBalance(cardNumber, cardCode, money){
-        try {
-            let user = this.checkUser(cardNumber, cardCode)
-            return user.balance + money
-        }
-        catch(err) {
-            return err
-        }
-    }
-
-    takeLoan(cardNumber, cardCode, loan){
-        try {
-            let user = this.checkUser(cardNumber, cardCode)
-            if(loan < user.balance){
-                return user.balance - loan
-            }
-            return user.balance
-        }
-        catch(err) {
-            return err
-        }
-    }
-
-    private checkUser(cardNumber, cardCode){
-        let currentUser = null
-        this.users.forEach(user => {
-            if(user.cardNumber === cardNumber && user.cardCode === cardCode){
-                currentUser = user
-            }
-        })
-        if(currentUser !== null){
-            return currentUser
-        }else{
-            throw "Invalid user"
-        }
-    }
-
+    return exists;
+  }
 };
--- bank-test.spec.ts
+++ bank-test.spec.ts
@@ -1,108 +1,189 @@
-import { Bank } from './bank-script'
-import { User } from './users'
+import { Bank } from "./bank-script";
+import { User } from "./users";
 
-describe('Bank', () => {
+describe("Bank", () => {
+  let users = [];
+  for (let i = 0; i < 5; i++) {
+    let cardNumber = "12344876" + i;
+    let cardCode = "123" + i;
+    let balance = 1200;
+    let name = "User" + i;
+    let user = new User(cardNumber, cardCode, balance, name);
+    users.push(user);
+  }
 
-    let users = []
-    for(let i = 0; i < 5; i++){
-        let cardNumber = "12344876" + i
-        let cardCode = "123" + i
-        let balance = 1200
-        let name = "User" + i
-        let user = new User(cardNumber, cardCode, balance, name)
-        users.push(user)
-    }
+  let bank = new Bank(users);
 
-    let bank = new Bank(users)
+  describe("Look balance", () => {
+    describe("Look balance with correct user", () => {
+      it.each([
+        [[users[0].cardNumber, users[0].cardCode], users[0].balance],
+        [[users[2].cardNumber, users[2].cardCode], users[2].balance],
+      ])("Card n°%i balance : %o", (n, expected) => {
+        expect(bank.lookBalance(n[0], n[1])).toBe(expected);
+      });
+    });
+    describe("Look balance with invalid user", () => {
+      it.each([
+        [[users[0].cardNumber + "1", users[0].cardCode], "Invalid user"],
+        [[users[0].cardNumber, users[0].cardCode + "1"], "Invalid user"],
+        [[users[0].cardNumber + "1", users[0].cardCode + "1"], "Invalid user"],
+        [[users[0].cardNumber + "A", users[0].cardCode + "B"], "Invalid user"],
+      ])("Invalid card number or card code provided.", (n, expected) => {
+        expect(bank.lookBalance(n[0], n[1])).toBe(expected);
+      });
+    });
+  });
 
-    describe('Look balance', () => {
-        describe('Look balance with correct user', () => {
-            it.each([
-                [[users[0].cardNumber, users[0].cardCode], users[0].balance],
-                [[users[2].cardNumber, users[2].cardCode], users[2].balance],
-            ])('Card n°%i balance : %o', (n, expected) => {
-                expect(bank.lookBalance(n[0], n[1])).toBe(expected)
-            })
-        })
-        describe('Look balance with invalid user', () => {
-            it.each([
-                [[users[0].cardNumber + "1", users[0].cardCode], "Invalid user"],
-                [[users[0].cardNumber, users[0].cardCode + "1"], "Invalid user"],
-                [[users[0].cardNumber + "1", users[0].cardCode + "1"], "Invalid user"],
-                [[users[0].cardNumber + "A", users[0].cardCode + "B"], "Invalid user"],
-            ])('Invalid card number or card code provided.', (n, expected) => {
-                expect(bank.lookBalance(n[0], n[1])).toBe(expected)
-            })
-        })
-    })
+  describe("Add balance", () => {
+    let countAdded = 0;
 
+    describe("Add balance with valid user", () => {
+      describe("Add balance > 0", () => {
+        countAdded = 200;
+        it(
+          "Card n°" +
+            users[0].cardNumber +
+            " balance : " +
+            (users[0].balance + countAdded) +
+            " with " +
+            countAdded +
+            " added",
+          () => {
+            expect(
+              bank.addBalance(
+                users[0].cardNumber,
+                users[0].cardCode,
+                countAdded
+              )
+            ).toBe(users[0].balance + countAdded);
+          }
+        );
+      });
+      describe("Add balance === 0", () => {
+        countAdded = 0;
+        it.each([
+          [[users[0].cardNumber, users[0].cardCode, 0], users[0].balance],
+        ])(
+          "Card n°%i balance : %o with " + countAdded + " added",
+          (n, expected) => {
+            expect(bank.addBalance(n[0], n[1], n[2])).toBe(expected);
+          }
+        );
+      });
+      describe("Add balance < 0", () => {
+        countAdded = -200;
+        it.each([
+          [[users[0].cardNumber, users[0].cardCode, 0], users[0].balance],
+        ])(
+          "Card n°%i balance : %o (can't add " +
+            countAdded +
+            " as a negative value)",
+          (n, expected) => {
+            expect(bank.addBalance(n[0], n[1], n[2])).toBe(expected);
+          }
+        );
+      });
+    });
+    describe("Add balance with invalid user", () => {
+      it.each([
+        [
+          [users[0].cardNumber + "1", users[0].cardCode + "1", countAdded],
+          "Invalid user",
+        ],
+      ])("Invalid card number or card code provided.", (n, expected) => {
+        expect(bank.addBalance(n[0], n[1], n[2])).toBe(expected);
+      });
+    });
+  });
 
-    describe('Add balance', () => {
-        let countAdded = 0
+  describe("Take a loan", () => {
+    let loan = 0;
 
-        describe('Add balance with valid user', () => {
-            describe('Add balance > 0', () => {
-                countAdded = 200
-                it('Card n°' + users[0].cardNumber + ' balance : ' + (users[0].balance + countAdded) +' with ' + countAdded + " added", () => {
-                    expect(bank.addBalance(users[0].cardNumber, users[0].cardCode, countAdded)).toBe(users[0].balance + countAdded)
-                })
-            })
-            describe('Add balance === 0', () => {
-                countAdded = 0
-                it.each(
-                    [[[users[0].cardNumber, users[0].cardCode, 0], users[0].balance],
-                ])('Card n°%i balance : %o with ' + countAdded + ' added', (n, expected) => {
-                    expect(bank.addBalance(n[0], n[1], n[2])).toBe(expected)
-                })
-            })
-            describe('Add balance < 0', () => {
-                countAdded = -200
-                it.each([
-                    [[users[0].cardNumber, users[0].cardCode, 0], users[0].balance],
-                ])('Card n°%i balance : %o (can\'t add ' + countAdded + ' as a negative value)', (n, expected) => {
-                    expect(bank.addBalance(n[0], n[1], n[2])).toBe(expected)
-                })
-            })
-        })
-        describe('Add balance with invalid user', () => {
-            it.each([
-                [[users[0].cardNumber + "1", users[0].cardCode + "1", countAdded], "Invalid user"],
-            ])("Invalid card number or card code provided.", (n, expected) => {
-                expect(bank.addBalance(n[0], n[1], n[2])).toBe(expected)
-            })
-        })
-    })
+    describe("Take a loan with correct user and loan < balance", () => {
+      loan = 200;
+      it.each([
+        [
+          [users[0].cardNumber, users[0].cardCode, loan],
+          users[0].balance - loan,
+        ],
+        [
+          [users[2].cardNumber, users[2].cardCode, loan],
+          users[2].balance - loan,
+        ],
+      ])(
+        "Card n°%i : You just loan " + loan + " , balance is now : %o",
+        (n, expected) => {
+          expect(bank.takeLoan(n[0], n[1], n[2])).toBe(expected);
+        }
+      );
+    });
+    describe("Take a loan with correct user and loan > balance", () => {
+      loan = 2000;
+      it.each([
+        [[users[0].cardNumber, users[0].cardCode, loan], users[0].balance],
+        [[users[2].cardNumber, users[2].cardCode, loan], users[2].balance],
+      ])(
+        "Can't take a loan for card n°%i balance : " + loan + " > %o",
+        (n, expected) => {
+          expect(bank.takeLoan(n[0], n[1], n[2])).toBe(expected);
+        }
+      );
+    });
+    describe("Take a loan with invalid user", () => {
+      it.each([
+        [[users[0].cardNumber + "1", users[0].cardCode, loan], "Invalid user"],
+        [[users[0].cardNumber, users[0].cardCode + "1", loan], "Invalid user"],
+        [
+          [users[0].cardNumber + "1", users[0].cardCode + "1", loan],
+          "Invalid user",
+        ],
+        [
+          [users[0].cardNumber + "A", users[0].cardCode + "B", loan],
+          "Invalid user",
+        ],
+      ])("Invalid card number or card code provided.", (n, expected) => {
+        expect(bank.takeLoan(n[0], n[1], n[2])).toBe(expected);
+      });
+    });
+  });
 
-    describe('Take a loan', () => {
-        let loan = 0;
+  describe("Add a user", () => {
+    let correctUser = new User("123456789", "123", 1200, "User");
+    let invalidUser = new User("123448760", "123", 1200, "User");
 
-        describe('Take a loan with correct user and loan < balance', () => {
-            loan = 200
-            it.each([
-                [[users[0].cardNumber, users[0].cardCode, loan], (users[0].balance - loan)],
-                [[users[2].cardNumber, users[2].cardCode, loan], (users[2].balance - loan)],
-            ])('Card n°%i : You just loan ' + loan + ' , balance is now : %o', (n, expected) => {
-                expect(bank.takeLoan(n[0], n[1], n[2])).toBe(expected)
-            })
-        })
-        describe('Take a loan with correct user and loan > balance', () => {
-            loan = 2000
-            it.each([
-                [[users[0].cardNumber, users[0].cardCode, loan], users[0].balance],
-                [[users[2].cardNumber, users[2].cardCode, loan], users[2].balance],
-            ])('Can\'t take a loan for card n°%i balance : '+ loan + ' > %o', (n, expected) => {
-                expect(bank.takeLoan(n[0], n[1], n[2])).toBe(expected)
-            })
-        })
-        describe('Take a loan with invalid user', () => {
-            it.each([
-                [[users[0].cardNumber + "1", users[0].cardCode, loan], "Invalid user"],
-                [[users[0].cardNumber, users[0].cardCode + "1", loan], "Invalid user"],
-                [[users[0].cardNumber + "1", users[0].cardCode + "1", loan], "Invalid user"],
-                [[users[0].cardNumber + "A", users[0].cardCode + "B", loan], "Invalid user"],
-            ])('Invalid card number or card code provided.', (n, expected) => {
-                expect(bank.takeLoan(n[0], n[1], n[2])).toBe(expected)
-            })
-        })
-    })
-})
+    describe("Add a user with correct user", () => {
+      it.each([[correctUser, true]])("User added", (n, expected) => {
+        expect(bank.addUser(n)).toBe(expected);
+      });
+    });
+    describe("Add a user with invalid user", () => {
+      it.each([[invalidUser, "User already exists"]])(
+        "Invalid user",
+        (n, expected) => {
+          expect(bank.addUser(n)).toBe(expected);
+        }
+      );
+    });
+  });
+
+  describe("Remove a user", () => {
+    let correctUser = new User("123456789", "123", 1200, "User");
+    let invalidUser = new User("123448760", "123", 1200, "User");
+
+    describe("Remove a user with correct user", () => {
+      it.each([[correctUser, true]])("User removed", (n, expected) => {
+        expect(bank.deleteUser(n)).toBe(expected);
+      });
+    });
+
+    describe("Remove a user with invalid user", () => {
+      it.each([[invalidUser, "Invalid user"]])(
+        "Invalid user",
+        (n, expected) => {
+          expect(bank.deleteUser(n)).toBe(expected);
+        }
+      );
+    });
+  });
+});
